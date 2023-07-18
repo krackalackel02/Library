889f312e2f62ef9e494f47eef427367b3c1b4ac2
Paul Kallarackel <paulkallarackel@gmail.com>
2023-07-18 16:45:54 +0100

    edit book functionality

--- script.js
+++ script.js
@@ -66,37 +66,6 @@ function updateDisplay() {
 
 updateDisplay();
 
-function removeBook(e) {
-	let book = e.target.closest(".book-card");
-	let removeButton = e.target.closest("button.remove");
-	if (!removeButton) return;
-
-	let indexToRemove = null;
-
-	myLibrary.list.forEach((element, index) => {
-		if (element.id !== book.id) return;
-		indexToRemove = index;
-	});
-
-	if (indexToRemove !== null) {
-		myLibrary.list.splice(indexToRemove, 1); // Remove the element at the specified index
-	}
-
-	console.log(myLibrary.list);
-	updateDisplay();
-}
-
-bookGrid.addEventListener("click", removeBook);
-const Popup = document.querySelector(".popup");
-
-function addBook(e) {
-	Popup.classList.toggle("active");
-}
-let addButton = document.querySelector(".add-book");
-addButton.addEventListener("click", addBook);
-let popupCloseButton = document.querySelector(".popup .remove");
-popupCloseButton.addEventListener("click", addBook);
-
 const formTitle = document.querySelector("input#title");
 const formAuthor = document.querySelector("input#author");
 const formRead = document.querySelector("input#read");
@@ -106,21 +75,113 @@ formPages.min = 0;
 const formAdd = document.querySelector("form button.submit");
 const Form = document.querySelector("form");
 
+// Function to reset the form to its initial state
+function resetForm() {
+	formTitle.value = "";
+	formAuthor.value = "";
+	formRead.checked = false;
+	formImg.value = "";
+	formPages.value = "";
+	formAdd.innerText = "Add to collection";
+	delete formAdd.dataset.bookId; // Remove the bookId attribute
+}
+
+// Function to show the popup and determine if it's an edit form or an add book form
+function togglePopup() {
+	Popup.classList.toggle("active");
+	if (Popup.classList.contains("editing")) {
+		Popup.classList.remove("editing");
+		formAdd.innerText = "Add to collection";
+		delete formAdd.dataset.bookId; // Remove the bookId attribute
+	}
+}
+
+function editBook(book) {
+	Popup.classList.toggle("active");
+	formTitle.value = book.title;
+	formAuthor.value = book.author;
+	formRead.checked = book.read;
+	formImg.value = book.img;
+	formPages.value = book.pages;
+	formAdd.innerText = "Edit Book";
+
+	// Store the book object to update after editing
+	formAdd.dataset.bookId = book.id;
+}
+function handleBook(e) {
+	let book = e.target.closest(".book-card");
+	let removeButton = e.target.closest("button.remove");
+	if (!book) return;
+	if (!removeButton) {
+		// Find the book in myLibrary.list based on its ID
+		let bookId = book.id;
+		let foundBook = myLibrary.list.find((b) => b.id === bookId);
+		if (foundBook) {
+			editBook(foundBook);
+		}
+	} else {
+		let indexToRemove = null;
+
+		myLibrary.list.forEach((element, index) => {
+			if (element.id !== book.id) return;
+			indexToRemove = index;
+		});
+
+		if (indexToRemove !== null) {
+			myLibrary.list.splice(indexToRemove, 1); // Remove the element at the specified index
+		}
+
+		console.log(myLibrary.list);
+		updateDisplay();
+	}
+}
+
+bookGrid.addEventListener("click", handleBook);
+const Popup = document.querySelector(".popup");
+
+let addButton = document.querySelector(".add-book");
+addButton.addEventListener("click", () => {
+	togglePopup();
+	resetForm(); // Reset the form when closing the popup
+});
+
+let popupCloseButton = document.querySelector(".popup .remove");
+popupCloseButton.addEventListener("click", () => {
+	togglePopup();
+	resetForm(); // Reset the form when closing the popup
+});
+
 formAdd.addEventListener("click", (e) => {
 	e.preventDefault();
-	if (!Form.checkValidity()) return; // Fix: use Form.checkValidity() instead of Form.valid
+	if (!Form.checkValidity()) return;
 
-	const newbook = new Book({
-		title: formTitle.value,
-		author: formAuthor.value,
-		pages: parseInt(formPages.value), // Convert to a numeric value
-		img: formImg.value, // Convert to a numeric value
-		read: formRead.checked, // Use the checked property to get a boolean value
-	});
+	const bookId = formAdd.dataset.bookId;
 
-	myLibrary.addBookToLibrary([newbook]);
+	if (bookId) {
+		// Editing existing book
+		let bookToUpdate = myLibrary.list.find((b) => b.id === bookId);
+		if (bookToUpdate) {
+			bookToUpdate.title = formTitle.value;
+			bookToUpdate.author = formAuthor.value;
+			bookToUpdate.pages = parseInt(formPages.value);
+			bookToUpdate.img = formImg.value;
+			bookToUpdate.read = formRead.checked;
+		}
+	} else {
+		// Adding new book
+		const newbook = new Book({
+			title: formTitle.value,
+			author: formAuthor.value,
+			pages: parseInt(formPages.value),
+			img: formImg.value,
+			read: formRead.checked,
+		});
+		myLibrary.addBookToLibrary([newbook]);
+	}
+
 	updateDisplay();
-    addBook()
+	togglePopup();
+	resetForm(); // Reset the form after submitting
 });
 
 function saveBook() {
@@ -132,29 +193,29 @@ saveButton.addEventListener("click", saveBook);
 
 function clearBook() {
 	myLibrary.list = [];
-    updateDisplay()
+	updateDisplay();
 }
 let clearButton = document.querySelector(".clear-book");
 clearButton.addEventListener("click", clearBook);
 
 function defaultBook() {
-    let initBook1 = new Book({
-        author: "J K Rowling",
-        title: "Harry Potter",
-        pages: 200,
-        read: false,
-        img: "Images/HP1.jpg",
-    });
-    let initBook2 = new Book({
-        author: "John Doe",
-        title: "The Adventure Begins",
-        pages: 250,
-        read: false,
-        img: "Images/TAB1.jpg",
-    });
-    myLibrary.addBookToLibrary([initBook1, initBook2]);
-    
-    updateDisplay()
+	let initBook1 = new Book({
+		author: "J K Rowling",
+		title: "Harry Potter",
+		pages: 200,
+		read: false,
+		img: "Images/HP1.jpg",
+	});
+	let initBook2 = new Book({
+		author: "John Doe",
+		title: "The Adventure Begins",
+		pages: 250,
+		read: false,
+		img: "Images/TAB1.jpg",
+	});
+	myLibrary.addBookToLibrary([initBook1, initBook2]);
+
+	updateDisplay();
 }
 let defaultButton = document.querySelector(".default-book");
 defaultButton.addEventListener("click", defaultBook);
